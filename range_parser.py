cdad9bfe561316a3daf8c0b868b3b05c155f4d80
enermis <fulgens.ailurus@gmail.com>
2019-12-22 23:06:39 +0100

    Map reduce instead of looping over results

--- range_parser.py
+++ range_parser.py
@@ -1,4 +1,4 @@
-from functools import cmp_to_key
+from functools import cmp_to_key, reduce
 
 codes_to_rank_map = {
     '2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7,
@@ -8,6 +8,15 @@ rank_to_codes_map = {
     0: '2', 1: '3', 2: '4', 3: '5', 4: '6', 5: '7', 6: '8', 7: '9',
     8: 'T', 9: 'J', 10: 'Q', 11: 'K', 12: 'A'
 }
+
+
+def compose(*functions):
+    """compose many functions into one"""
+
+    def compose_two(f, g):
+        return lambda x: f(g(x))
+
+    return reduce(compose_two, functions, lambda x: x)
 
 
 def compare_rank(a, b):
@@ -103,9 +112,6 @@ def parse_sub_range(sub_range):
 
 
 def parse_range(hand_range):
-    result = set()
-
-    for sub_range in hand_range.split(','):
-        result = result.union(parse_sub_range(sub_range.strip()))
-
-    return sorted(list(result), key=cmp_to_key(compare_rank))
+    result_lists = map(compose(parse_sub_range, str.strip), hand_range.split(','))
+    hands = set(reduce(list.__add__, result_lists, []))
+    return sorted(list(hands), key=cmp_to_key(compare_rank))
