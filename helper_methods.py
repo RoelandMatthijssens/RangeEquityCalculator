5dbc3526c8a7ba304aefe2af4ae23315dc94c4bd
enermis <fulgens.ailurus@gmail.com>
2019-12-23 02:06:59 +0100

    Use reduce instead of flatten
    This gets rid of the helper_methods.py file

--- helper_methods.py
+++
@@ -1,2 +0,0 @@
-def flatten(l):
-    return [i for sub_l in l for i in sub_l]
--- range_parser.py
+++ range_parser.py
@@ -1,13 +1,12 @@
-from functools import cmp_to_key
+from functools import cmp_to_key, reduce
 
 from .hand import Hand
-from .helper_methods import flatten
 from .range import Range
 
 
 def parse_range(ranges):
     ranges = [Range(i.strip()) for i in ranges.split(',')]
-    hands = flatten([hand_range.hands for hand_range in ranges])
+    hands = reduce(list.__add__, [hand_range.hands for hand_range in ranges])
     unique_hands = list(set(hands))
     sorted_hands = sorted(unique_hands, key=cmp_to_key(Hand.compare))
     return [hand.value for hand in sorted_hands]
