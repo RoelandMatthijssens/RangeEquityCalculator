137429fc56641a277e792829da776cf2329f252e
enermis <fulgens.ailurus@gmail.com>
2019-12-23 22:38:27 +0100

    remove redundant main file

--- main.py
+++
@@ -1,7 +0,0 @@
-from arg_parser import parse_args
-from range_parser import parse_range
-
-if __name__ == '__main__':
-    input_range = parse_args()
-    hands = parse_range(input_range)
-    print(hands)
--- range_parser.py
+++ range_parser.py
@@ -1,5 +1,6 @@
 from functools import cmp_to_key, reduce
 
+from arg_parser import parse_args
 from hand import Hand
 from range import Range
 
@@ -10,3 +11,9 @@ def parse_range(ranges):
     unique_hands = list(set(hands))
     sorted_hands = sorted(unique_hands, key=cmp_to_key(Hand.compare))
     return [hand.value for hand in sorted_hands]
+
+
+if __name__ == '__main__':
+    input_range = parse_args()
+    hands = parse_range(input_range)
+    print(hands)
